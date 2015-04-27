125159a822a1ab2170a626facc3553e707b865d4
Mike Silvis <mikesilvis@gmail.com>
2015-04-27 08:52:57 -0700

    fixing nba linescores

--- lib/espn_scraper/score.rb
+++ lib/espn_scraper/score.rb
@@ -103,12 +103,12 @@ module ESPN
             game_info[:home_team_record] = competitor['records'].first['summary']
             game_info[:home_team_name]   = competitor['team']['shortDisplayName']
             game_info[:home_team]        = competitor['team']['abbreviation'].downcase
-            game_info[:home_score]       = competitor['statistics'][score_index]['displayValue'].to_i unless game_info[:state] == 'pregame' rescue byebug
+            game_info[:home_score]       = competitor['statistics'][score_index]['displayValue'].to_i unless game_info[:state] == 'pregame' rescue competitor['linescores'].map { |a| a['value'] }.inject{|sum,x| sum + x }
           else
             game_info[:away_team_record] = competitor['records'].first['summary']
             game_info[:away_team_name]   = competitor['team']['shortDisplayName']
             game_info[:away_team]        = competitor['team']['abbreviation'].downcase
-            game_info[:away_score]       = competitor['statistics'][score_index]['displayValue'].to_i unless game_info[:state] == 'pregame'
+            game_info[:away_score]       = competitor['statistics'][score_index]['displayValue'].to_i unless game_info[:state] == 'pregame' rescue competitor['linescores'].map { |a| a['value'] }.inject{|sum,x| sum + x }
           end
         end
 
